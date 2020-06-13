8005a43950fe71c6fbc2f756a745902a139920bd
tzatushevskaya <52919759+tzatushevskaya@users.noreply.github.com>
2020-06-14 02:00:49 +0300

    HW5 version 2

--- ConvertTemperature.java
+++ ConvertTemperature.java
@@ -3,27 +3,7 @@ public class ConvertTemperature {
 	
 	public static void main(String[] args) {
 	
-	double n = Celsius.toReaumur(1);
-	System.out.printf("%.2f", n);
-	Celsius.toFahrenheit(1);
-	Celsius.toRankine(1);
-	Celsius.toKelvin(1);
-	Reaumur.toCelsius(1);
-	Reaumur.toFahrenheit(1);
-	Reaumur.toRankine(1);
-	Reaumur.toKelvin(1);
-	Fahrenheit.toReaumur(1);
-	Fahrenheit.toCelsius(1);
-	Fahrenheit.toRankine(1);
-	Fahrenheit.toKelvin(1);
-	Rankine.toReaumur(1);
-	Rankine.toFahrenheit(1);
-	Rankine.toCelsius(1);
-	Rankine.toKelvin(1);
-	Kelvin.toReaumur(1);
-	Kelvin.toFahrenheit(1);
-	Kelvin.toCelsius(1);
-	Kelvin.toRankine(1);
+		Quick.calculation(1.0, "C", "K");
 
 	}
 
@@ -31,102 +11,176 @@ public class ConvertTemperature {
 
 class Celsius {
 	
-	static double toReaumur (int value) {
-		
-		return (value * 0.8) ;
+	static double toReaumur (double value) {		
+		return value * 0.8;
 	}
 	
-	static void toFahrenheit (float value) {
-		System.out.println(value + " C = " + value * 1.8 + 32 + " F");
+	static double toFahrenheit (double value) {		
+		return (value * 1.8 + 32);
 	}
 	
-	static void toRankine (float value) {
-		System.out.println(value + " C = " + (value + 273.15) * 1.8 + " R");
+	static double toRankine (double value) {
+		return (value + 273.15) * 1.8;
 	}
 	
 	
-	static void toKelvin (float value) {
-		System.out.println(value + " C = " + value + 273.15 + " K");
+	static double toKelvin (double value) {
+		return (value + 273.15);
 	}
 		
 }
 
 class Reaumur {
 	
-	static void toCelsius (float value) {
-		System.out.println(value + " Re = " + value / 0.8 + " C");
+	static double toCelsius (double value) {
+		return  value / 0.8;
 	}
 	
-	static void toFahrenheit (float value) {
-		System.out.println(value + " Re = " + value * 2.25 + 32 + " F");
+	static double toFahrenheit (double value) {
+		return value * 2.25 + 32;
 	}
 	
-	static void toRankine (float value) {
-		System.out.println(value + " Re = " + value * 2.25 + 491.67 + " R");
+	static double toRankine (double value) {
+		return value * 2.25 + 491.67;
 	}
 	
-	static void toKelvin (float value) {
-		System.out.println(value + " Re = " + value / 0.8 + 273.15 + " K");
+	static double toKelvin (double value) {
+		return value / 0.8 + 273.15;
 	}
 		
 }
 
 class Fahrenheit {
 	
-	static void toCelsius (float value) {
-		System.out.println(value + " F = " + (value - 32)/1.8 + " C");
+	static double toCelsius (double value) {
+		return (value - 32)/1.8;
 	}
 	
-	static void toReaumur (float value) {
-		System.out.println(value + " F = " + (value - 32)/1.8 * 0.8 + " Re");
+	static double toReaumur (double value) {
+		return (value - 32)/1.8 * 0.8;
 	}
 	
-	static void toRankine (float value) {
-		System.out.println(value + " F = " + (value - 32 + 491.67) + " R");
+	static double toRankine (double value) {
+		return value - 32 + 491.67;
 	}
 	
-	static void toKelvin (float value) {
-		System.out.println(value + " F = " + (value - 32)/1.8 + 273.15 + " K");
+	static double toKelvin (double value) {
+		return (value - 32)/1.8 + 273.15;
 	}
 		
 }
 
 class Rankine {
 	
-	static void toCelsius (float value) {
-		System.out.println(value + " R = " + (value / 1.8 - 273.15) + " C");
+	static double toCelsius (double value) {
+		return value / 1.8 - 273.15;
 	}
 	
-	static void toReaumur (float value) {
-		System.out.println(value + " R = " + (value / 1.8 - 273.15) * 0.8 + " Re");
+	static double toReaumur (double value) {
+		return (value / 1.8 - 273.15) * 0.8;
 	}
 	
-	static void toFahrenheit (float value) {
-		System.out.println(value + " R = " + (value - 459.67) + " F");
+	static double toFahrenheit (double value) {
+		return value - 459.67;
 	}
 	
-	static void toKelvin (float value) {
-		System.out.println(value + " R = " + value / 1.8 + " K");
+	static double toKelvin (double value) {
+		return value / 1.8;
 	}
 		
 }
 
 class Kelvin {
 	
-	static void toCelsius (float value) {
-		System.out.println(value + " K = " + (value - 273.15) + " C");
+	static double toCelsius (double value) {
+		return value - 273.15;
 	}
 	
-	static void toReaumur (float value) {
-		System.out.println(value + " K = " + (value - 273.15) * 0.8 + " Re");
+	static double toReaumur (double value) {
+		return value - 273.15;
 	}
 	
-	static void toFahrenheit (float value) {
-		System.out.println(value + " K = " + (value - 273.15) * 1.8 + 32 + " F");
+	static double toFahrenheit (double value) {
+		return (value - 273.15) * 1.8 + 32;
 	}
 	
-	static void toRankine (float value) {
-		System.out.println(value + " K = " + (value * 1.8) + " R");
+	static double toRankine (double value) {
+		return value * 1.8;
 	}	
 		
 }
+
+class Quick {
+	
+	static void calculation (double temp1, String initUnit, String finalUnit) {
+		String s = initUnit + finalUnit;
+		double temp2 = 0.00;
+		switch (s) {
+		case "CRe":
+			temp2 = Celsius.toReaumur(temp1);			
+			break;
+		case "CF":
+			temp2 = Celsius.toFahrenheit(temp1);	
+			break;
+		case "CR":
+			temp2 = Celsius.toRankine(temp1);
+			break;
+		case "CK":
+			temp2 = Celsius.toKelvin(temp1);
+			break;
+		case "ReC":
+			temp2 = Reaumur.toCelsius(temp1);			
+			break;
+		case "ReF":
+			temp2 = Reaumur.toFahrenheit(temp1);	
+			break;
+		case "ReR":
+			temp2 = Reaumur.toRankine(temp1);
+			break;
+		case "ReK":
+			temp2 = Reaumur.toKelvin(temp1);
+			break;
+		case "FRe":
+			temp2 = Fahrenheit.toReaumur(temp1);			
+			break;
+		case "FC":
+			temp2 = Fahrenheit.toCelsius(temp1);	
+			break;
+		case "FR":
+			temp2 = Fahrenheit.toRankine(temp1);
+			break;
+		case "FK":
+			temp2 = Fahrenheit.toKelvin(temp1);
+			break;
+		case "RRe":
+			temp2 = Rankine.toReaumur(temp1);			
+			break;
+		case "RF":
+			temp2 = Rankine.toFahrenheit(temp1);	
+			break;
+		case "RC":
+			temp2 = Rankine.toCelsius(temp1);
+			break;
+		case "RK":
+			temp2 = Rankine.toKelvin(temp1);
+			break;
+		case "KRe":
+			temp2 = Kelvin.toReaumur(temp1);			
+			 break;
+		case "KF":
+			temp2 = Kelvin.toFahrenheit(temp1);	
+			break;
+		case "KR":
+			temp2 = Kelvin.toRankine(temp1);
+			break;
+		case "KC":
+			temp2 = Kelvin.toCelsius(temp1);
+			break;
+			}
+		System.out.print(temp1 + " " + initUnit + " = ");
+		System.out.printf("%.2f", temp2);
+		System.out.println(" " + finalUnit);	
+	}
+	
+}
+
