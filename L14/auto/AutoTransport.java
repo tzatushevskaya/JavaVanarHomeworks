c3b0c8491211bf9811f9bef7f71bb251b0e4b29d
tzatushevskaya <52919759+tzatushevskaya@users.noreply.github.com>
2020-07-08 23:08:05 +0300

    Add files via upload

---
+++ L14/auto/AutoTransport.java
@@ -0,0 +1,9 @@
+package auto;
+
+public class AutoTransport extends Transport {
+
+	public AutoTransport() {
+		super("land");
+	}
+
+}
---
+++ L14/auto/AviaTransport.java
@@ -0,0 +1,9 @@
+package auto;
+
+public class AviaTransport extends Transport {
+
+	public AviaTransport() {
+		super("air");
+	}
+	
+}
