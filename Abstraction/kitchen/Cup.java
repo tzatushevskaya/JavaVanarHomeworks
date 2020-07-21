068742c6390129512faf40766eb9042e13ed46ee
tzatushevskaya <52919759+tzatushevskaya@users.noreply.github.com>
2020-07-21 16:37:21 +0300

    Add files via upload

---
+++ Abstraction/kitchen/Cup.java
@@ -0,0 +1,17 @@
+package kitchen;
+
+import liquids.Juice;
+
+public class Cup {
+
+	private Juice content;
+
+	public Juice getContent() {
+		return content;
+	}
+
+	public void setContent(Juice content) {
+		this.content = content;
+	}
+	
+}
