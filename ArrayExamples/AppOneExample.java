7bad8f78adad842651d5940ced1dc8a0d130d696
tzatushevskaya <52919759+tzatushevskaya@users.noreply.github.com>
2020-08-04 23:47:04 +0300

    Add files via upload

---
+++ ArrayExamples/AppOneExample.java
@@ -0,0 +1,29 @@
+package main;
+
+public class AppOneExample {
+	
+	public static void main(String[] args) {
+		
+		// Arrays, Vectors, Multi/Uni-dimensional, ..., Collection
+		
+		// C Style
+		
+		// Java style
+		
+		// declaration
+		
+		int[] values = { 10, 20, 30, 40, 50 }; // variable -> array object Array
+					 //   0,  1,  2, .....
+		
+		// class with array utils Arrays
+		
+		
+		
+		System.out.println ("The \"values\" variable is of type " + values.getClass().getName());
+		
+		for (int i=0; i < values.length; i++) {
+			System.out.println ("[" + i + "]" + values[i]);
+		}
+	}
+
+}
